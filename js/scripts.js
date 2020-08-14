673ba65a232ca41ce394dcdd863bc00afc041950
Jaki Widby <jaki.widby@gmail.com>
2020-08-14 09:01:24 -0700

    create click function to hide intro and show questions section

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,23 @@
-function determineResult(val1, val2, val3, val4, val5, val6) {
-  const quizTotal = val1 + val2 + val3 + val4 + val5 +val6; 
-  let result;
+// function determineResult(val1, val2, val3, val4, val5, val6) {
+//   const quizTotal = val1 + val2 + val3 + val4 + val5 +val6; 
+//   let result;
 
-  if () {
-    result = "Python";
-  } else if () {
-    result= "Ruby";
-  } else if() {
-    result = "C#";
-  } else {
-    result = "JavaScript";
-  }
-}
+//   if () {
+//     result = "Python";
+//   } else if () {
+//     result= "Ruby";
+//   } else if() {
+//     result = "C#";
+//   } else {
+//     result = "JavaScript";
+//   }
+// }
+
+$(document).ready(function() {
+  $("#continue1").click(function(event) {
+    event.preventDefault();
+    $("#intro").hide();
+    $("#questions").show();
+  });
+
+});
