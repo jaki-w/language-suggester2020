e60ee6519e43936cc159c2ffedd5a5d385d323a8
Jaki Widby <jaki.widby@gmail.com>
2020-08-14 08:53:33 -0700

    create determineResult function

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function determineResult(val1, val2, val3, val4, val5, val6) {
+  const quizTotal = val1 + val2 + val3 + val4 + val5 +val6; 
+  let result;
+
+  if () {
+    result = "Python";
+  } else if () {
+    result= "Ruby";
+  } else if() {
+    result = "C#";
+  } else {
+    result = "JavaScript";
+  }
+}
